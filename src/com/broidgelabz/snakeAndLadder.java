291269879d3164dfb1ce0e68b05f20c653749762
adityabhartib <adityabharti.ab@gmail.com>
2022-10-14 18:23:52 +0530

    Welcome msg Added

---
+++ src/com/broidgelabz/snakeAndLadder.java
@@ -0,0 +1,7 @@
+package com.broidgelabz;
+
+public class snakeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("------Welcome to Snake and Ladder Problem-----------------");
+    }
+}
