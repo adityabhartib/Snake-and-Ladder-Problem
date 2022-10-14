b6ed9845875993a1388b106dba804f26ba423fa9
adityabhartib <adityabharti.ab@gmail.com>
2022-10-14 19:17:57 +0530

    UC5 number of time dice roll to win Added

--- src/com/broidgelabz/snakeAndLadder.java
+++ src/com/broidgelabz/snakeAndLadder.java
@@ -2,36 +2,30 @@ package com.broidgelabz;
 
 public class snakeAndLadder {
     public static void main(String[] args) {
-        System.out.println("----------------Welcome to Snake and Ladder Problem-----------------");
-        int startPosition = 0, position = 0,WINNING_POSTION=100;
+        int count = 0; // number of times die was rolled
+        int count1s = 0; // number of times 1 was rolled
+        int count2s = 0; // number of times 2 was rolled
+        int count3s = 0;
+        int count4s = 0;
+        int count5s = 0;
+        int count6s = 0;
 
-        while (position < WINNING_POSTION) {
+        while (count < 100) {
+            count1s = (int) (Math.random( )*6 + 1);
+            count2s = (int) (Math.random( )*6 + 1);
+            count3s = (int) (Math.random( )*6 + 1);
+            count4s = (int) (Math.random( )*6 + 1);
+            count5s = (int) (Math.random( )*6 + 1);
+            count6s = (int) (Math.random( )*6 + 1);
 
-            System.out.println("Start position is: " + startPosition);
-            int diceNumber = (int) Math.floor(Math.random() * 10) % 6 + 1;
-            System.out.println("Dice Number is : " + diceNumber);
-
-            int option = (int) Math.floor(Math.random() * 10) % 3;
-            System.out.println("Player option is : " + option);
-
-            switch (option) {
-                case 1:
-                    position = diceNumber + position;
-                    System.out.println("Ladder");
-                    System.out.println("Position for the player after the ladder is : " + position);
-                    break;
-
-                case 2:
-                    if (position - diceNumber <= 0)
-                        position = 0;
-                    else
-                        position = position - diceNumber;
-                    System.out.println("Snack");
-                    System.out.println("Position for the player after the Snake is : " + position);
-                    break;
-                default:
-                    System.out.println("No play");
-            }
+            count++;
         }
+        System.out.println("Number of times the Dice was rolled: "+ count);
+        System.out.println("Number of times 1 was rolled: " + count1s);
+        System.out.println("Number of times 2 was rolled: " + count2s);
+        System.out.println("Number of times 3 was rolled: " + count3s);
+        System.out.println("Number of times 4 was rolled: " + count4s);
+        System.out.println("Number of times 5 was rolled: " + count5s);
+        System.out.println("Number of times 6 was rolled: " + count6s);
     }
 }
