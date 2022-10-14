6c2d0f139c13d031ef3206f833860fcccd979bec
adityabhartib <adityabharti.ab@gmail.com>
2022-10-14 18:28:55 +0530

    UC1 start position Zero

--- src/com/broidgelabz/snakeAndLadder.java
+++ src/com/broidgelabz/snakeAndLadder.java
@@ -3,5 +3,7 @@ package com.broidgelabz;
 public class snakeAndLadder {
     public static void main(String[] args) {
         System.out.println("------Welcome to Snake and Ladder Problem-----------------");
+        int startPosition = 0;
+        System.out.println(" Start position is: " + startPosition);
     }
 }
